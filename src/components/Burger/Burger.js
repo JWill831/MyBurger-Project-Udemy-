17a373b1e7039041114f32f7e758f818e342b92d
JWill831 <jon.fogtown@gmail.com>
2021-01-04 15:10:08 -0800

    129 outputting burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import classes from './Burger.module.css'
 import BurgerIngrediant from './BurgerIngrediant/BurgerIngrediant';
-const Burger = (props) => {
 
+
+
+const Burger = (props) => {
+    const transformedIngredients=Object.keys(props.ingredients)
+    .map(igKey=> {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngrediant key={igKey+i} type={igKey}/>
+        });
+    });
 
     return (
         <div className={classes.Burger}>
+
             <BurgerIngrediant type="bread-top"/>
-            <BurgerIngrediant type="cheese"/>
-            <BurgerIngrediant type="meat"/>
+            {transformedIngredients}
             <BurgerIngrediant type="bread-bottom"/>
             
         </div>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React , {useState} from 'react';
 import Auxilliary from '../../hoc/Auxilliary';
 import Burger from '../../components/Burger/Burger';
 
+// const [burgerState, setBurgerState]= new useState();
+const BurgerState={
+    ingredients: {
+        salad: 1,
+        bacon: 1,
+        cheese: 2,
+        meat: 2
+    }
+};
+
+
 const BurgerBuilder = () => {
+    
+    
     return (
         <div>
             <Auxilliary>
-                <Burger/>
+                <Burger ingredients={BurgerState.ingredients}/>
                 <div>Build Controls</div>
             </Auxilliary>
         </div>
