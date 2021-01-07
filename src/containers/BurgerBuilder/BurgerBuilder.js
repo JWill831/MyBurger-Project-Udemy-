735f5e018b1339a3b26a1e43bd0c30525aa4635b
JWill831 <jon.fogtown@gmail.com>
2021-01-07 13:33:58 -0800

    133 disabled less button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,70 +1,73 @@
-import React, {useState} from 'react';
-import Auxilliary from '../../hoc/Auxilliary';
+import React, { useState } from 'react';
+import Auxillary from '../../hoc/Auxilliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 
-const BurgerBuilder = () => {
-    const [burgerState, setBurgerState]=  useState(
-        {
-                ingredients: {
-                    salad: 1,
-                    bacon: 2,
-                    cheese: 0,
-                    meat: 0,
-                },
-                totalPrice: 4
-            }
-    );
-    console.log("burgerState",burgerState)
-    
-    
-    // setBurgerState = {
-    //     ingredients: {
-    //         salad: 0,
-    //         bacon: 0,
-    //         cheese: 0,
-    //         meat: 0,
-    //     },
-    //     totalPrice: 4
-    // };
-    
-    
-    
-    const INGREDIENT_PRICES= {
-        salad:0.5,
-        cheese:0.4,
-        meat:1.3,
-        bacon:0.7
-    };
-    
-    const addIngredientHandler = (type)=>{
-        const oldCount=burgerState.ingredients[type];
-        const updateCount=oldCount+1;
-        const updatedIngredients={
-            ...burgerState.ingredients
-        };
-        updatedIngredients[type]=updateCount;
-        const priceAddition= INGREDIENT_PRICES[type];
-        const oldPrice=burgerState.totalPrice;
-        const newPrice = oldPrice +priceAddition;
-        setBurgerState({totalPrice: newPrice, indredients: updatedIngredients})
-    
-    }
-    
-    const removeIngredientHandler = (type)=>{
-        
-    }
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+const BurgerBuilder = (props) => {
 
-    
+    const [state, setState] = useState({
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    });
+
+    const addIngredientHandler = (type) => {
+        const oldCount = state.ingredients[type];
+        const updatedCounted = oldCount + 1;
+        const updatedIngredients = {
+            ...state.ingredients
+        };
+        updatedIngredients[type] = updatedCounted;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+
+    };
+
+    const removeIngredientHandler = (type) => {
+        const oldCount = state.ingredients[type];
+        if (oldCount<=0){
+            return;
+        }
+        const updatedCounted = oldCount - 1;
+        const updatedIngredients = {
+            ...state.ingredients
+        };
+        updatedIngredients[type] = updatedCounted;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = state.totalPrice;
+        const newPrice = oldPrice + priceDeduction;
+        setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    };
+    const disabledInfo={
+        ...state.ingredients
+    };
+    for (let key in disabledInfo){
+        disabledInfo[key]= disabledInfo[key]<=0
+    };
     return (
-        <div>
-            <Auxilliary>
-                <Burger ingredients={burgerState.ingredients}/>
-                <BuildControls/>
-            </Auxilliary>
-        </div>
-    )
-}
+        <Auxillary>
+            <Burger ingredients={state.ingredients} />
+            <BuildControls  
+            ingredientAdded={addIngredientHandler} 
+            ingredientDeducted={removeIngredientHandler} 
+            disabled={disabledInfo}/>
+        </Auxillary>
+
+    );
+};
 
 export default BurgerBuilder;
+
